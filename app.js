c79378aa73c73f9968d0705f0116b972af8f8c3f
Roberto Rajaonarivony <robertogasy@gmail.com>
2023-05-19 20:22:18 +0300

    null

--- app.js
+++ app.js
@@ -1,31 +1,38 @@
-const path = require('path'); 
-const dotenv = require('dotenv').config()
-
-
+// Import required modules
+const path = require('path');
+const dotenv = require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 
+// Import controllers and models
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
-const MONGODB_URI = `${process.env.MONGODB_URI}`;
+// Set up MongoDB URI
+const MONGODB_URI = process.env.MONGODB_URI;
 
+// Create express app
 const app = express();
+
+// Set up session store
 const store = new MongoDBStore({
   uri: MONGODB_URI,
   collection: 'sessions'
 });
 
+// Configure view engine and views directory
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+// Import routes
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 
+// Middleware and session configuration
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
@@ -37,6 +44,7 @@ app.use(
   })
 );
 
+// Set user data in request if available in session
 app.use((req, res, next) => {
   if (!req.session.user) {
     return next();
@@ -49,27 +57,19 @@ app.use((req, res, next) => {
     .catch(err => console.log(err));
 });
 
+// Register routes
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
+// Handle 404 errors
 app.use(errorController.get404);
 
+// Connect to MongoDB and start the server
 mongoose
   .connect(MONGODB_URI)
   .then(result => {
-    User.findOne().then(user => {
-      if (!user) {
-        const user = new User({
-          name: 'Roberto',
-          email: 'max@test.com',
-          cart: {
-            items: []
-          }
-        });
-        user.save();
-      }
-    });
+   
     app.listen(3000);
   })
   .catch(err => {
